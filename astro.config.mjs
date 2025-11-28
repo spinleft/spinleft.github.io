87bf0114ac70fc71ddf948b6a46652d90d3a62e8
spinleft <spinleftgit@gmail.com>
2025-11-28 20:51:05 +0800

    publish site to github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
  * @Author: spinleft spinleftgit@gmail.com
  * @Date: 2025-11-28 19:34:03
  * @LastEditors: spinleft spinleftgit@gmail.com
- * @LastEditTime: 2025-11-28 20:29:18
+ * @LastEditTime: 2025-11-28 20:50:31
  * @FilePath: \spinleft.github.io\astro.config.mjs
  * @Description: 
  * 
@@ -13,5 +13,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.spinleft.com',
+    site: 'https://spinleft.github.io',
 });
