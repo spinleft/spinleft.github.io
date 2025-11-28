2657d386ba135061d7305e109561a770e2ed6c4d
spinleft <spinleftgit@gmail.com>
2025-11-28 20:10:59 +0800

    deploy site from github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
+/*
+ * @Author: spinleft spinleftgit@gmail.com
+ * @Date: 2025-11-28 19:34:03
+ * @LastEditors: spinleft spinleftgit@gmail.com
+ * @LastEditTime: 2025-11-28 20:01:30
+ * @FilePath: \spinleft.github.io\astro.config.mjs
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+ */
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://spinleft.github.io',
+});
